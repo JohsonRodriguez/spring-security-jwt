4dc5324256c14cea077201bacf7c9224337a8629
Johson Rodriguez <sukerjc@gmail.com>
2021-03-12 23:57:09 -0500

    inicio

---
+++ src/test/java/com/byron/resources/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byron.resources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
